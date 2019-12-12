4481288cda24ac2c774a489e970d9f29e295ef47
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-12 21:12:10 +0200

    pavyko

--- Uzduotis4/Uzduotis4/Program.cs
+++ Uzduotis4/Uzduotis4/Program.cs
@@ -10,6 +10,25 @@ namespace Uzduotis4
     {
         static void Main(string[] args)
         {
+            string mokykloPavadinimas = "CodingAcademy";
+            var kursoPavadinimas = "IT08";
+            int studentuSkaicius = 12;
+
+            DateTime kursoPradziosData = new DateTime(2019, 12, 11);
+            DateTime KursoPabaigosData = new DateTime(2020, 02, 29);
+            
+            double skirtumasTarpPradziosIrPabaigos = (KursoPabaigosData - kursoPradziosData).TotalDays;
+
+
+            DateTime siandienosData = DateTime.Today;
+
+            Console.WriteLine("Mokyklos pavadinimas: {0}", mokykloPavadinimas);
+            Console.WriteLine("Kurso pavadinimas: {0}", kursoPavadinimas);
+            Console.WriteLine("Studentu Skaicius: {0}", studentuSkaicius);
+            Console.WriteLine("Siandienos data: {0}", siandienosData.ToLongDateString());
+            Console.WriteLine("Kurso trukme {0}", skirtumasTarpPradziosIrPabaigos);
+
+            Console.ReadLine();
         }
     }
 }
