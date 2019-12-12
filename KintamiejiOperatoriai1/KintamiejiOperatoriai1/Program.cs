c571472d15c1669317c449df3bf0296850f0af89
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-12 21:43:42 +0200

    baigta

--- KintamiejiOperatoriai1/KintamiejiOperatoriai1/Program.cs
+++ KintamiejiOperatoriai1/KintamiejiOperatoriai1/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace KintamiejiOperatoriai1
 {
+    
+        
     class Program
     {
         static void Main(string[] args)
         {
+            int skaicius;
+
+            Console.WriteLine("Ivesti skaiciu: ");
+            skaicius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("skaicius {0}", skaicius);
+            Console.ReadLine();
+              
         }
     }
 }
