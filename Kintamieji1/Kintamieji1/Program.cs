e33986f5bc52f5269f52bba2126f333909c50973
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-12 19:54:37 +0200

    pavyko

---
+++ Kintamieji1/Kintamieji1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kintamieji1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mokykloPavadinimas = "CodingAcademy";
+            var kursoPavadinimas = "IT08";
+            int studentuSkaicius = 12;
+
+            DateTime siandienosData = DateTime.Today;
+
+            Console.WriteLine("Mokyklos pavadinimas: {0}", mokykloPavadinimas);
+            Console.WriteLine("Kurso pavadinimas: {0}", kursoPavadinimas);
+            Console.WriteLine("Studentu Skaicius: {0}", studentuSkaicius);
+            Console.WriteLine("Siandienos data: {0}", siandienosData.ToLongDateString());
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Pamoka2.1/Pamoka2.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pamoka2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yearOfBirth;
+            string firstName;
+
+            var lastName = "Pavardenis"; //var-nesvarbu kokio tipo duomenis ivesime, nuskaitys pati programa
+            decimal salary = 1999.9m;
+            DateTime lastPayDay = DateTime.Today.AddMonths(-1);
+
+            yearOfBirth = 1990;
+            firstName = "Vardenis";
+
+            Console.WriteLine("Vardas, Pavarde: {0}, {1}", firstName, lastName);
+            Console.WriteLine("Gimimo metai: {0}", yearOfBirth);
+            Console.WriteLine("Pajamos: {0}", salary);
+            Console.WriteLine("Paskutini karta ismoketa alga: {0}", lastPayDay.ToLongDateString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
