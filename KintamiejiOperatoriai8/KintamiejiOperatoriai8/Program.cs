b716c19bf4ff3b72732cffd66b9334984ff4d4a0
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-16 12:36:11 +0200

    veikia

---
+++ KintamiejiOperatoriai8/KintamiejiOperatoriai8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KintamiejiOperatoriai8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string vardas = " ";
+            string pavarde = " ";
+
+            Console.WriteLine("Ivesti Varda: ");
+            vardas = Console.ReadLine();
+            Console.WriteLine("Ivesti Pavarde: ");
+            pavarde = Console.ReadLine();
+
+
+            Console.WriteLine("{0} {1}", vardas, pavarde);
+
+            Console.ReadLine();
+         }
+    }
+}
