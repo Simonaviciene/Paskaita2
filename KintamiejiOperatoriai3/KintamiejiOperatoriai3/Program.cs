c303cee8ca9c3338d820729a235a83ba08b392b7
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-16 10:31:43 +0200

    3

---
+++ KintamiejiOperatoriai3/KintamiejiOperatoriai3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KintamiejiOperatoriai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int skaicius;
+            int skaicius1;
+
+            if (skaicius == skaicius1)
+            {
+                Console.WriteLine("true");
+            }
+            else 
+                {
+                    Console.WriteLine("false");
+                }
+               
+            
+
+
+            Console.WriteLine("Ivesti du skaicius: ");
+            skaicius = Convert.ToInt32(Console.ReadLine());
+            skaicius1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("skaicius {0}", skaicius);
+            Console.WriteLine("skaicius {0}", skaicius1);
+            Console.ReadLine();
+        }
+    }
+}
