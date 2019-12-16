646adab5e333fa425664be691e76d4d78de456dd
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-16 10:00:27 +0200

    veikia

---
+++ KintamiejiOperatoriai22/KintamiejiOperatoriai22/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KintamiejiOperatoriai2
+{
+    class Program
+    {
+        static void SwapNum(ref int x, ref int y) 
+        {
+            int tempswap = x;
+            x = y;
+            y = tempswap;
+        }
+        
+        static void Main(string[] args)
+        {
+            int A = 1;
+            int B = 15;
+
+            Console.WriteLine("Reiksmes pries keitima");
+            Console.WriteLine();
+            Console.WriteLine("A=" + " " + A);
+            Console.WriteLine("B=" + " " + B);
+            SwapNum(ref A, ref B);
+            Console.WriteLine();
+            Console.WriteLine("Reiksmes po pakeitimo");
+            Console.WriteLine();
+            Console.WriteLine("A=" + " " + A);
+            Console.WriteLine("B=" + " " + B);
+            Console.WriteLine();
+            Console.ReadLine();
+
+
+        }
+    }
+}
