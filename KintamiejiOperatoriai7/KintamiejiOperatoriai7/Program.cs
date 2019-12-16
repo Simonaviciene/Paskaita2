89ee380d2c08d8decbc01a8a588d4a2c94384097
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-16 12:05:18 +0200

    veikia

---
+++ KintamiejiOperatoriai7/KintamiejiOperatoriai7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KintamiejiOperatoriai7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int skaicius;
+            
+            Console.WriteLine("Ivesti pirma skaiciu: ");
+            skaicius = Convert.ToInt32(Console.ReadLine());
+        
+            if (skaicius < 0)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+                Console.ReadLine();
+        }
+    }
+}
