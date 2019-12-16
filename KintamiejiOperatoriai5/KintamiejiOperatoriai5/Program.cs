d1974929b332df14a09facd1896275e16c490466
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-16 11:20:57 +0200

    Veikia

---
+++ KintamiejiOperatoriai5/KintamiejiOperatoriai5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KintamiejiOperatoriai5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ilgis;
+            double plotis;
+                
+            Console.WriteLine("Ivesti staciakampio ilgi: ");
+            ilgis = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ivesti staciakampio ploti: ");
+            plotis = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Staciakampio plotas:");
+            Console.WriteLine(ilgis * plotis);
+           
+
+            Console.ReadLine();
+        }
+    }
+}
