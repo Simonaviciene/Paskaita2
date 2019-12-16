f3d81272ba64ccd201300317d6c08ae4b035c930
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-16 11:05:59 +0200

    veikia

--- KintamiejiOperatoriai4/KintamiejiOperatoriai4/Program.cs
+++ KintamiejiOperatoriai4/KintamiejiOperatoriai4/Program.cs
@@ -10,6 +10,21 @@ namespace KintamiejiOperatoriai4
     {
         static void Main(string[] args)
         {
+            int A;
+            int B;
+
+            Console.WriteLine("Ivesti pirma skaiciu: ");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ivesti antra skaiciu: ");
+            B = Convert.ToInt32(Console.ReadLine());
+
+           
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(++A);
+            Console.WriteLine(--B);
+
+            Console.ReadLine();
         }
     }
 }
