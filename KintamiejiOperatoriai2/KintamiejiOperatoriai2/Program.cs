d78b2f226f311cfb763145cc0cd0141607ddf06d
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-16 09:47:18 +0200

    baigta

--- KintamiejiOperatoriai2/KintamiejiOperatoriai2/Program.cs
+++ KintamiejiOperatoriai2/KintamiejiOperatoriai2/Program.cs
@@ -10,6 +10,22 @@ namespace KintamiejiOperatoriai2
     {
         static void Main(string[] args)
         {
+            int A = 1;
+            int B = 15;
+
+            Console.WriteLine("Reiksmes pries keitima");
+            Console.WriteLine();
+            Console.WriteLine("A=" + " " + A);
+            Console.WriteLine("B=" + " " + B);
+            SwapNum(ref A, ref B);
+            Console.WriteLine();
+            Console.WriteLine("Reiksmes po pakeitimo");
+            Console.WriteLine();
+            Console.WriteLine("A=" + " " + A);
+            Console.WriteLine("B=" + " " + B);
+            Console.WriteLine();
+            Console.ReadLine();
+
 
         }
     }
