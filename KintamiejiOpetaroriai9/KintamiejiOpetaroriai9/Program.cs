14f394c0d9c510ecfedf30d7f7083c097e7ab6c6
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-16 15:21:00 +0200

    neveikia

---
+++ KintamiejiOpetaroriai9/KintamiejiOpetaroriai9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KintamiejiOpetaroriai9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string miestoPavadinimas = "Vilnius";
+            string gatvesPavadinimas = "S. Zukausko g.";
+            int namoNumeris = 21;
+            decimal namoPastatymoMetai = 2004m ;
+
+            Console.WriteLine("{0} {1} {2} {3}", miestoPavadinimas, gatvesPavadinimas, namoNumeris);
+            Console.ReadLine();
+        }
+    }
+}
