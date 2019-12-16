d6ce77a973c5b6715e6215e7d937a15fe52d3778
Vilija <Simonaviciene@users.noreply.github.com>
2019-12-16 11:53:51 +0200

    neveikia

---
+++ KintamiejiOperatoriai6/KintamiejiOperatoriai6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KintamiejiOperatoriai6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string TextBox;
+
+            Console.WriteLine("Ivesti teksto eilute: {0}", TextBox);
+            {
+
+                if (!string.IsNullOrWhiteSpace(TextBox.Text))
+                {
+                    Console.WriteLine("Eilute tuscia -> True");
+                }  
+                else
+                {                    {
+                    Console.WriteLine("Eilute tuscia -> False");
+                }
+                
+                    Console.ReadLine();
+                }
+            }   
+        }
+    }      
+}
